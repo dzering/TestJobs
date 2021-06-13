6592c24c0584260d3ca954c2073cc35596353380
Sergey <svigolubev@gmail.com>
2021-06-13 18:45:11 +0400

    Add project Test jobs

---
+++ TestSimbirSoft/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+
+namespace TestSimbirSoft
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClientService wcs = new WebClientService();
+            Stream stream = wcs.GetPage("https://www.simbirsoft.com/");
+
+            HtmlProcessing hp = new HtmlProcessing();
+            string text = hp.GetText(stream);
+
+            Console.WriteLine(text);
+
+            TextHandler th = new TextHandler();
+            Dictionary<string, int> dictionary = th.GetWordsStatistic(text);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("{0} = {1}", item.Key, item.Value);
+            }
+
+            Console.WriteLine(dictionary.Count);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TestSimbirSoft/WebClientService.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Net;
+
+namespace TestSimbirSoft
+{
+    internal class WebClientService
+    {
+        private string GetFileName(string url)
+        {
+            string name = null;
+            string[] urlSplit = url.Split('/');
+            string lastElArray = urlSplit[urlSplit.Length - 1];
+            if (lastElArray == "")
+            {
+                name = "index.html";
+            }
+            return name;
+        }
+
+        /// <summary>
+        /// Получает страницу из интернета. Сохраняет в файл.
+        /// </summary>
+        /// <param name="url">Полный url страницы для скачивания</param>
+        /// <returns></returns>
+        public Stream GetPage(string url)
+        {
+            string fileName = GetFileName(url);
+
+            if (!File.Exists(fileName))
+            {
+                WebClient webClient = new WebClient();
+
+                Stream stream = webClient.OpenRead(url);
+
+                using (FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write))
+                {
+                    stream.CopyTo(fs);
+                }
+            }
+
+            StreamReader sr = new StreamReader(fileName);
+            return sr.BaseStream;
+        }
+    }
+}
